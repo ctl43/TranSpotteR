0392f6bac08f87362b69ae3fb3a77c8f192dcf39
ctl43 <ctlaw@hku.hk>
2020-11-25 16:10:28 +0800

    Using the in-house OLC assembler to replace the combination of velvet and CAP3 for speed enhancement.

--- R/sequence_construction.R
+++ R/sequence_construction.R
@@ -1,9 +1,19 @@
-sequence_construction <- function(x){
-  sign <- runValue(strand(x)) 
+#' @export
+#' @importFrom GenomicRanges GRanges split
+#' @importFrom S4Vectors 'elementMetadata<-' Rle runValue elementMetadata
+#' @importFrom IRanges subsetByOverlaps IntegerList
+#' @importFrom csaw mergeWindows
+#' @importFrom GenomeInfoDb seqinfo
+#' @importFrom Biostrings DNAStringSet DNAStringSetList reverseComplement
+#' @importFrom BiocParallel bplapply MulticoreParam
+
+sequence_construction <- function(x, BPPARAM = MulticoreParam(workers = 20)){
+  sign <- runValue(strand(x))
   if(length(sign)!=1){
     stop()
   }
-  info <- do.call(rbind, x$partners)
+
+  info <- S4Vectors::elementMetadata(unlist(x$partners))
   partner_seq <- info$SEQUENCE
   partner_seq <- DNAStringSet(partner_seq)
   if(sign=="+"){
@@ -11,48 +21,34 @@ sequence_construction <- function(x){
   }else{
     partner_seq[info$is_rc] <- reverseComplement(partner_seq[info$is_rc])
   }
-  
-  grp <- rep(seq_along(x$partners), sapply(x$partners, nrow))
+  partner_seq <- as.character(partner_seq)
+  grp <- rep(seq_along(x$partners), lengths(x$partners))
   grp <- factor(grp, levels = seq_along(x$partners))
   partner_seq <- split(partner_seq, grp)
-  
+
   # Assembling partner seq
-  partner_contigs <- DNAStringSetList(mclapply(partner_seq, function(y).assembly(y)[[2]], mc.cores = 60L))
-  
+  partner_contigs <- bplapply(partner_seq, greedy_scs, BPPARAM = BPPARAM)
+
   # Assembling cluster sequences
   grp <- rep(seq_along(x$members), lengths(x$members))
   grp <- factor(grp, levels = seq_along(x$members))
-  cluster_seq <- split(DNAStringSet(unlist(x$members)$SEQUENCE), grp)
-  cluster_contigs <- DNAStringSetList(mclapply(cluster_seq, function(y).assembly(y)[[2]], mc.cores = 60L))
+  cluster_seq <- split(unlist(x$members)$SEQUENCE, grp)
+  cluster_contigs <- bplapply(cluster_seq, greedy_scs, BPPARAM = BPPARAM)
 
   # Assembling both
-  merged_seq <- mapply(function(x,y)c(x,y),x=partner_contigs, y=cluster_contigs)
-  merged_contigs <- DNAStringSetList(mclapply(merged_seq, function(y).assembly(y)[[2]], mc.cores = 60L))
-  
-  # Attempting to constructing the break point between LINE1 and genome
-  if(sign=="+"){
-    combined_contigs <- DNAStringSetList(mcmapply(join_reads, p=cluster_contigs, q=partner_contigs, min_overlap=10, mc.cores = 3L)) # Be careful about the direction
-  }else{
-    combined_contigs <- DNAStringSetList(mcmapply(join_reads, p=partner_contigs, q=cluster_contigs, min_overlap=10, mc.cores = 3L)) # Be careful about the direction
-  }
-
-  # Finding the longer reads as final assembled reads
-  pmax_len <- pmax(max(nchar(cluster_contigs)), max(nchar(partner_contigs)))
-  is_improved <- max(nchar(merged_contigs))>(pmax_len+30)
-  has_combined <- lengths(combined_contigs)!=0
-  merged_contigs[!is_improved] <- DNAStringSetList(DNAStringSet())
-  merged_better <- max(nchar(merged_contigs))>=max(nchar(combined_contigs))
-  selected_contigs <- rep(DNAStringSetList(DNAStringSet()), length(merged_better))
-  selected_contigs[merged_better] <- merged_contigs[merged_better]
-  selected_contigs[!merged_better] <- combined_contigs[!merged_better]
-  has_longer <- lengths(selected_contigs)>0
+  merged_seq <- mapply(function(x,y)c(x,y),x = partner_contigs, y=cluster_contigs, SIMPLIFY = FALSE)
+  merged_contigs <- bplapply(merged_seq, greedy_scs, BPPARAM = BPPARAM)
 
   # Storing data
-  names(cluster_contigs) <- names(partner_contigs) <- names(selected_contigs) <- seq_along(x)
-  x$long_contigs <- selected_contigs
-  partner_contigs[has_longer] <- DNAStringSetList(DNAStringSet())
+  names(cluster_contigs) <- names(partner_contigs) <- names(merged_contigs) <- seq_along(x)
+  partner_contigs <- DNAStringSetList(lapply(partner_contigs, DNAStringSet)) # to preserve the sequence names
+  cluster_contigs <- DNAStringSetList(lapply(cluster_contigs, DNAStringSet))
+  merged_contigs <- DNAStringSetList(lapply(merged_contigs, DNAStringSet))
+  has_long <- lengths(merged_contigs) > 0
+  x$long_contigs <- merged_contigs
+  partner_contigs[has_long] <- DNAStringSetList(DNAStringSet())
   x$partner_contigs <- partner_contigs
-  cluster_contigs[has_longer] <- DNAStringSetList(DNAStringSet())
+  cluster_contigs[has_long] <- DNAStringSetList(DNAStringSet())
   x$cluster_contigs <- cluster_contigs
   x
 }
