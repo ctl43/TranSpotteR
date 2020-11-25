fe81af54e0f30241dfe8d6d483f4a1e1d7026821
ctl43 <ctlaw@hku.hk>
2020-11-25 16:11:46 +0800

    Removing excessive filters and added namespace information.

--- R/filter_read_by_coverage.R
+++ R/filter_read_by_coverage.R
@@ -1,43 +1,31 @@
-filter_read_by_coverage <- function(x, min_read=2, min_peak=5, window_tol=500, rescue_tol=500, size_tol=0, rescue=FALSE){
+#' @export
+#' @importFrom GenomicRanges GRanges
+#' @importFrom S4Vectors 'elementMetadata<-' Rle
+#' @importFrom IRanges coverage slice subsetByOverlaps IntegerList
+#' @importFrom csaw mergeWindows
+#' @importFrom GenomeInfoDb seqinfo
+
+filter_read_by_coverage <- function(x, min_read = 2,
+                                    min_peak = 5,
+                                    window_tol = 500,
+                                    rescue_tol = 500,
+                                    rescue = FALSE){
   cvg <- coverage(x)
   sliced <- slice(cvg, min_read)
-  max_count <- lapply(sliced, max)
-  irange <- unlist(IRangesList(lapply(sliced, function(x)x@ranges)))
-  collapsed <- GRanges(names(irange), irange, seqinfo=seqinfo(x))
+  max_count <- IntegerList(lapply(sliced, max))
+  chr <- Rle(names(sliced), lengths(sliced))
+  collapsed <- GRanges(seqnames = chr,
+                       IRanges(unlist(start(sliced)), unlist(end(sliced))),
+                       seqinfo = seqinfo(x))
   collapsed$max_count <- unlist(max_count)
-  peaks <- collapsed[collapsed$max_count>=min_peak]
-  
+  peaks <- collapsed[collapsed$max_count >= min_peak]
   if(rescue){
-    temp <- collapsed[!collapsed$max_count>=min_peak] # Recusing reads that are around the main clusters
+    temp <- collapsed[!collapsed$max_count >= min_peak] # Recusing reads that are around the main clusters
     rescued <- subsetByOverlaps(temp, peaks + rescue_tol)
     collapsed <- c(peaks, rescued)
   }else{
     collapsed <- peaks
   }
-  
-  collapsed <- csaw::mergeWindows(collapsed, tol=window_tol)$regions # Merge cluster within the tolerated range
-  
-  ol <- findOverlaps(x, collapsed)
-  collapsed$count <- as.integer(table(ol@to))
-  collapsed <- collapsed[width(collapsed)>size_tol] # small size usually indicate the overrepresentation of PCR duplicate
-  
-  # Identifying the peaks of reads clusters
-  splitted <- split(collapsed, as.factor(seqnames(collapsed)))
-  splitted <- mapply(function(x,y)Views(x,y@ranges),x=cvg, y=splitted)
-
-  find_peak <- function(x){
-    val <- runValue(x)
-    len <- runLength(x)
-    max_value <- max(val)
-    max_loc <- which(val==max_value)
-    max_loc <- max_loc[which.max(len[max_loc])]
-    c(as.integer(len[max_loc]/2)+sum(len[1:(max_loc-1)]), max_value)
-  }
-  
-  peaks <- lapply(splitted, function(x)t(sapply(as.vector(x), find_peak)))
-  peaks <- peaks[lengths(peaks)>0]
-  peaks <- do.call(rbind, peaks)
-  collapsed$peaks <- peaks[,1]
-  collapsed$peak_count <- peaks[,2]
+  collapsed <- mergeWindows(collapsed, tol = window_tol)$regions # Merge cluster within the tolerated range
   collapsed
 }
