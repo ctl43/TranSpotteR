5e6be04cb12ffb035e2314b68174c1352c82c100
ctl43 <ctlaw@hku.hk>
2020-11-25 20:31:55 +0800

    Wrapping the import of discordant reads and multiple mapped reads together.

--- R/import_files.R
+++ R/import_files.R
@@ -1,9 +1,33 @@
 #' @export
-#' @importFrom GenomicRanges GRanges
+#' @importFrom GenomicRanges GRanges seqnames
 #' @importFrom data.table fread
 #' @importFrom S4Vectors 'elementMetadata<-'
+#' @importFrom IRanges start end CharacterList IntegerList
 
-import_files <- function(x, mapq_filter=NULL, include_unmapped=TRUE){
+import_files <- function(mm, disc, include_unmapped = TRUE, disc_min_mapq = 20, anchor_min_mapq = 5){
+  # Total multiple mapped reads
+  mm_gr <- .internal_import(x = mm)
+
+  # Determine split reads (multi and unique mapped reads are on the same reads)
+  mm_gr$is_anchor <- mm_gr$MAPQ >= anchor_min_mapq & !mm_gr$is_supp
+  mm_gr$is_disc <- FALSE
+
+  # For discordant reads
+  disc_gr <- .internal_import(x = disc, mapq_filter = disc_min_mapq)
+  seqname <- CharacterList(split(as.character(seqnames(disc_gr)), disc_gr$QNAME))
+  multiple_seqnames <- lengths(unique(seqname))>1
+  start_range <- IntegerList(split(start(disc_gr), disc_gr$QNAME))
+  start_range <- range(start_range)
+  keep <- rownames(start_range)[(start_range[,2] - start_range[,1] > 5000)|multiple_seqnames] # Extracting read that are far apart enough
+  disc_gr <- disc_gr[disc_gr$QNAME%in%keep]
+  disc_gr$is_anchor <- FALSE
+  disc_gr$is_disc <- TRUE
+  out <- c(mm_gr, disc_gr)
+  names(out) <- out$QNAME_id
+  out
+}
+
+.internal_import <- function(x, mapq_filter = NULL, include_unmapped=TRUE){
   what <- c("character","integer","character", "integer", "integer", "character","character")
   x <- do.call(rbind, lapply(x, fread, colClasses=what))
   x$is_first <- !!bitwAnd(x$FLAG, 0x40)
