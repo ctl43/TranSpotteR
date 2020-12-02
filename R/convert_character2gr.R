16311407afac9b62a0b438688c46595b88de1fd7
ctl43 <ctlaw@hku.hk>
2020-12-02 18:10:04 +0800

    Changed to input format for integration.

--- R/convert_character2gr.R
+++ R/convert_character2gr.R
@@ -2,18 +2,19 @@
 #' @importFrom GenomicRanges GRanges
 #' @importFrom S4Vectors split
 #' @importFrom IRanges IRanges
-
+#' @importFrom IRanges CharacterList
 
 # Converting read annotation to genomic range
 convert_character2gr <- function(y){
+  y <- CharacterList(strsplit(y, " "))
   y <- y[grepl(":", y)]
   grp <- factor(rep(seq_along(y), lengths(y)), levels = seq_along(y))
   y <- unlist(y)
   y <- do.call(rbind, strsplit(y, ":|-"))
-  y[,4][y[,4]==""] <- "-" # adding back the negative strand
+  y[, 4][y[, 4] == ""] <- "-" # adding back the negative strand
   y <- data.frame(y)
-  y[,2] <- as.integer(y[,2])
-  y[,3] <- as.integer(y[,3])
-  gr <- GRanges(seqnames=y[,1], IRanges(start=y[,2], end=y[,3]), strand = y[,4])
+  y[, 2] <- as.integer(y[, 2])
+  y[, 3] <- as.integer(y[, 3])
+  gr <- GRanges(seqnames=y[, 1], IRanges(start = y[, 2], end = y[, 3]), strand = y[, 4])
   split(gr, grp)
 }
