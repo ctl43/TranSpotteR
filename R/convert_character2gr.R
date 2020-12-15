bafc2b4cb21f13e12f5707330ab8de610c73ad0f
ctl43 <ctlaw@hku.hk>
2020-12-15 18:18:25 +0800

    Converted as an utils script

--- R/convert_character2gr.R
+++ R/convert_character2gr.R
@@ -6,10 +6,19 @@
 
 # Converting read annotation to genomic range
 convert_character2gr <- function(y){
+
   is_list <- class(y)%in%c("list", "CompressedCharacterList")
   if(is_list){
     list_grp <- factor(rep(seq_along(y), lengths(y)), levels = seq_along(y))
     y <- unlist(y, use.names = FALSE)
+  }
+
+  if(length(y) == 0 & is_list){
+    return(GRangesList(GRanges()))
+  }
+
+  if(length(y) == 0 & !is_list){
+    return(GRanges())
   }
 
   if(any(!grepl(":", y))){
@@ -28,4 +37,65 @@ convert_character2gr <- function(y){
   }else{
     return(gr)
   }
+}
+
+#' @export
+#' @importFrom S4Vectors runValue runLength
+rle_to_cigar <- function(x){
+  rl <- runLength(x)
+  rv <- runValue(x)
+  what <- class(x)
+  if(what =="CompressedRleList"){
+    grp <- rep(seq_along(rv), lengths(rv))
+    converted <- aggregate(paste0(unlist(rl), unlist(rv)), list(grp), paste, collapse="")[,2]
+  }
+
+  if(what == "Rle"){
+    converted <- paste(paste0(rl, rv), collapse = "")
+  }
+  return(converted)
+}
+
+#' @export
+#' @importFrom S4Vectors runValue runLength aggregate
+#' @importFrom GenomicAlignments cigarToRleList
+
+unify_cigar_strand <- function(cigar, flag = NULL, from, to, along_query = FALSE){
+  out <- rep("", length(cigar))
+  is_unmapped <- cigar == "*"
+  if(all(is_unmapped)){
+    return(cigar)
+  }
+  cigar <- cigar[!is_unmapped]
+  flag <- flag[!is_unmapped]
+  if(!is.null(flag)){
+    from <- ifelse(bitwAnd(flag, 0x10), "-", "+")
+  }
+  cigar <- cigarToRleList(cigar)
+  is_reversed <- from != to
+  cigar[is_reversed] <- revElements(cigar[is_reversed])
+  rv <- runValue(cigar)
+  rl <- runLength(cigar)
+  out[!is_unmapped] <- rle_to_cigar(cigar)
+  out[is_unmapped] <- "*"
+  out
+}
+
+
+#' @export
+#' @importFrom GenomicRanges GRanges
+#' @importFrom IRanges IRanges
+#' @importFrom GenomicAlignments cigarWidthAlongReferenceSpace
+
+
+sam2gr <- function(mapping, seqinfo=NULL){
+  if(is.null(mapping)){
+    return(GRanges())
+  }
+  gr <- GRanges(seqnames = mapping$RNAME,
+                IRanges(mapping$POS, width=cigarWidthAlongReferenceSpace(mapping$CIGAR)),
+                strand = ifelse(bitwAnd(mapping$FLAG, 0x10), "-", "+"),
+                seqinfo = seqinfo)
+  names(gr) <- mapping$QNAME
+  gr
 }
