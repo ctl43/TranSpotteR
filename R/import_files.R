b405956f967fb35709f42a00d5fe840c34aa5f0a
ctl43 <ctlaw@hku.hk>
2020-12-16 22:35:13 +0800

    Reorganised and sped up a bit.

--- R/import_files.R
+++ R/import_files.R
@@ -3,61 +3,53 @@
 #' @importFrom data.table fread
 #' @importFrom S4Vectors 'elementMetadata<-'
 #' @importFrom IRanges start end CharacterList IntegerList
-#' @importFrom BiocParallel bplapply MulticoreParam
+#' @importFrom  GenomicAlignments cigarWidthAlongReferenceSpace
 
 import_files <- function(mm, disc, include_unmapped = TRUE,
                          disc_min_mapq = 20, anchor_min_mapq = 5){
   # Total multiple mapped reads
-  mm_gr <- .internal_import(x = mm)
-
+  mm_dt <- .internal_import(x = mm)
   # Determine split reads (multi and unique mapped reads are on the same reads)
-  mm_gr$is_anchor <- mm_gr$MAPQ >= anchor_min_mapq & !mm_gr$is_supp
-  mm_gr$is_disc <- FALSE
+  mm_dt$is_anchor <- mm_dt$MAPQ >= anchor_min_mapq & !mm_dt$is_supp
+  mm_dt$is_disc <- FALSE
 
   # For discordant reads
-  disc_gr <- .internal_import(x = disc, mapq_filter = disc_min_mapq)
-  seqname <- CharacterList(split(as.character(seqnames(disc_gr)), disc_gr$QNAME))
-  multiple_seqnames <- lengths(unique(seqname)) > 1
-  start_range <- IntegerList(split(start(disc_gr), disc_gr$QNAME))
-  start_range <- range(start_range)
-  keep <- rownames(start_range)[(start_range[,2] - start_range[,1] > 5000)|multiple_seqnames] # Extracting read that are far apart enough
-  disc_gr <- disc_gr[disc_gr$QNAME%in%keep]
-  disc_gr$is_anchor <- FALSE
-  disc_gr$is_disc <- TRUE
-  out <- c(mm_gr, disc_gr)
-  names(out) <- out$QNAME_id
-  out
+  disc_dt <- .internal_import(x = disc)
+  has_multiple_seqnames <- lengths(unique(CharacterList(split(disc_dt$RNAME, disc_dt$QNAME)))) > 1
+  start_range <- range(IntegerList(split(disc_dt$POS, disc_dt$QNAME)))
+  keep <- rownames(start_range)[(start_range[,2] - start_range[,1] > 5000) | has_multiple_seqnames] # Extracting read that are far apart enough
+  disc_dt <- disc_dt[disc_dt$QNAME %in% keep,]
+  disc_dt$is_anchor <- FALSE
+  disc_dt$is_disc <- TRUE
+  to_keep <- disc_dt$QNAME[disc_dt$MAPQ > disc_min_mapq]
+  disc_dt <- disc_dt[disc_dt$QNAME %in% to_keep, ]
+  dt <- rbind(mm_dt, disc_dt)
+  dt <- convertingHtoS(dt) # Converting all hard clipped to soft clipped
+  dt[dt$CIGAR=="*", ]$RNAME <- "UNMAPPED"
+  dt[dt$CIGAR=="*", ]$POS <- 1
+  dt[dt$CIGAR=="*", ]$CIGAR <- paste0(nchar(dt$SEQUENCE[dt$CIGAR=="*"]),"M")
+  seq_info <- readRDS("/home/ctlaw/dicky/reference/hs37d5_seq_info.rds")
+  seq_info@seqnames <- c(seq_info@seqnames, "UNMAPPED")
+  seq_info@seqlengths <- c(seq_info@seqlengths, 99999L)
+  seq_info@is_circular <- c(seq_info@is_circular, FALSE)
+  seq_info@genome <- c(seq_info@genome, "hs37d5_KJ173426")
+  gr <- GRanges(seqnames = dt$RNAME,
+                IRanges(dt$POS, width = cigarWidthAlongReferenceSpace(dt$CIGAR)),
+                strand = ifelse(bitwAnd(dt$FLAG, 16), "-", "+"),
+                seqinfo = seq_info)
+  names(gr) <- dt$QNAME_id
+  elementMetadata(gr) <- dt
+  gr
 }
 
-.internal_import <- function(x, mapq_filter = NULL,
-                             include_unmapped = TRUE){
+#' @export
+#' @importFrom data.table rbindlist fread
+.internal_import <- function(x){
   what <- c("character","integer","character", "integer", "integer", "character", "character")
-  x <- do.call(rbind, lapply(x, fread, colClasses = what))
+  x <- rbindlist(lapply(x, fread, colClasses = what))
   x$is_first <- !!bitwAnd(x$FLAG, 0x40)
   x$is_rc <- !!bitwAnd(x$FLAG, 0x10)
   x$is_supp <- !!bitwAnd(x$FLAG, 0x800)
-  x$QNAME_id <- paste0(x$QNAME,"_",as.integer(!x$is_first)+1)
-
-  # Converting all hard clipped to soft clipped
-  x <- convertingHtoS(x)
-  hs37d5_seq_info <- readRDS("/home/ctlaw/dicky/reference/hs37d5_seq_info.rds")
-  if(include_unmapped){
-    hs37d5_seq_info@seqnames <- c(hs37d5_seq_info@seqnames, "UNMAPPED")
-    hs37d5_seq_info@seqlengths <- c(hs37d5_seq_info@seqlengths, 99999L)
-    hs37d5_seq_info@is_circular <- c(hs37d5_seq_info@is_circular, FALSE)
-    hs37d5_seq_info@genome <- c(hs37d5_seq_info@genome, "hs37d5_KJ173426")
-    x[x$CIGAR=="*", ]$RNAME <- "UNMAPPED"
-    x[x$CIGAR=="*", ]$POS <- 1
-    x[x$CIGAR=="*", ]$CIGAR <- paste0(nchar(x[x$CIGAR=="*", ]$SEQUENCE),"M")
-  }
-  gr <- sam2gr(x, seqinfo=hs37d5_seq_info)
-  elementMetadata(gr) <- x
-
-  if(!is.null(mapq_filter)){
-    discard <- gr$QNAME[gr$MAPQ<mapq_filter]
-    gr <- gr[!gr$QNAME%in%discard, ]
-    gr
-  }else{
-    gr
-  }
+  x$QNAME_id <- paste0(x$QNAME,"_",as.integer(!x$is_first) + 1)
+  x
 }
