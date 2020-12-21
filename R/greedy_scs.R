bff5245a8f6d1fe3630222c3cdfacdc8868db30b
ctl43 <ctlaw@hku.hk>
2020-12-21 19:16:46 +0800

    Converted most stroage to data.table and avoid repeatedly calculating the pairwise identity.

--- R/greedy_scs.R
+++ R/greedy_scs.R
@@ -1,13 +1,15 @@
 #' @export
-#' @importFrom data.table data.table
+#' @importFrom data.table data.table setDT
 #' @importFrom Biostrings consensusMatrix DNAStringSet
 #' @importFrom uuid UUIDgenerate
 
 #' @export
-greedy_scs <- function(vec, n_reads = NULL, msa_result = FALSE, return_no_assembly = FALSE, add_id = TRUE)
-# It assembles reads (getting the shortest common superstring problem, scs) by overlap-layout-consensus method.
-# The overlapping part, it simply chooses the pair with longest overlapping length, so called a greedy way.
-# Written by Cheuk-Ting Law
+greedy_scs <- function(vec, n_reads = NULL, msa_result = FALSE,
+                         return_no_assembly = FALSE, add_id = TRUE, min_len = 8L, min_pid = 85,
+                         run_msa = TRUE)
+  # It assembles reads (getting the shortest common superstring problem, scs) by overlap-layout-consensus method.
+  # The overlapping part, it simply chooses the pair with longest overlapping length, so called a greedy way.
+  # Written by Cheuk-Ting Law
 {
   # Dealing with NULL vector
   if(is.null(vec)){
@@ -19,29 +21,49 @@ greedy_scs <- function(vec, n_reads = NULL, msa_result = FALSE, return_no_assemb
       return(consensus)
     }
   }
-
+  # vec <- sim_frag
   if(is.null(n_reads)){
     n_reads <- rep(1L, length(vec))
   }
 
-  names(n_reads) <- names(vec) <- seq_along(vec)
+  idx <- names(n_reads) <- names(vec) <- seq_along(vec)
   original_vec <- vec
 
-  # This can be vastly improved by not calculating the overlaps that have been calculated before
-  # Will be implemented into C++ in the future
+  info <- data.table(t(combn(names(vec), 2)))
+  colnames(info) <- c("seq1", "seq2")
+  info <- data.table(info, setDT(overlapper(vec[info[[1]]], vec[info[[2]]])))
+  info <- .reorder_seq_info(info)
+  # Filtering abberent alignment
+  info <- info[info$pid > min_pid & info$aln_ol_len > min_len & (info$seq1_ol_start == 0L | info$seq2_ol_start == 0L), ]
+  info <- info[order(info$aln_ol_len, decreasing = TRUE), ]
 
-  # Creating a drafted assembled sequence for MSA by greedy shortest supersequence (SCS)
-  for(i in seq_along(vec)){ # Maximum number of loops
-    ol <- .overlap_reads(vec)
-    if(nrow(ol)>0){
-      selected <- ol[1L, ]
-      new_seq <- paste0(substr(vec[[selected$seq1]], 1L, selected$seq1_ol_start - 1L),
-                        substr(vec[[selected$seq2]], selected$seq2_ol_start, selected$seq2_len))
-      names(new_seq) <- paste0(selected$seq1, "_", selected$seq2)
-      vec <- c(new_seq, vec[!names(vec)%in%c(selected$seq1, selected$seq2)])
-    }else{
-      break
-    }
+  # Converting to 1-based index
+  info$seq1_ol_start <- info$seq1_ol_start + 1L
+  info$seq1_ol_end <- info$seq1_ol_end + 1L
+  info$seq2_ol_start <- info$seq2_ol_start + 1L
+  info$seq2_ol_end <- info$seq2_ol_end + 1L
+  setkeyv(info, c("seq1", "seq2"))
+
+  while(nrow(info) > 0){
+    selected <- info[1L, ]
+    left_idx <- selected$seq1
+    right_idx <- selected$seq2
+    new_seq <- paste0(substr(vec[left_idx], 1L, selected$seq1_ol_start - 1L),
+                      substr(vec[right_idx], selected$seq2_ol_start, selected$seq2_len))
+    names(new_seq) <- paste0(selected$seq1, "_", selected$seq2)
+    vec <- vec[!names(vec) %in% c(selected$seq1, selected$seq2)]
+    ol <- overlapper(rep(new_seq, length(vec)), vec)
+    suppressWarnings(ol <- data.table(data.table(seq1 = names(new_seq), seq2 = names(vec)), ol))
+    ol <- ol[ol$pid > min_pid & ol$aln_ol_len > min_len & (ol$seq1_ol_start == 0L | ol$seq2_ol_start == 0L), ]
+    ol <- .reorder_seq_info(ol)
+    ol$seq1_ol_start <- ol$seq1_ol_start + 1L
+    ol$seq1_ol_end <- ol$seq1_ol_end + 1L
+    ol$seq2_ol_start <- ol$seq2_ol_start + 1L
+    ol$seq2_ol_end <- ol$seq2_ol_end + 1L
+    vec <- c(new_seq, vec)
+    info <- info[!(seq1 %in% c(left_idx, right_idx) | seq2 %in% c(left_idx, right_idx))]
+    info <- rbindlist(list(ol, info))
+    info <- info[order(info$aln_ol_len, decreasing = TRUE), ]
   }
 
   # Picking out those read that did not assemble
@@ -49,11 +71,16 @@ greedy_scs <- function(vec, n_reads = NULL, msa_result = FALSE, return_no_assemb
   solo_id <- ass_id[!grepl("_", ass_id)]
   non_solo_id <- grep("_", ass_id, value = TRUE)
   member_idx <- strsplit(non_solo_id, "_")
-  msa_out <- mapply(function(x,y)t_coffee(c(original_vec[x],y)),
-                    x = member_idx, y = vec[non_solo_id], SIMPLIFY = FALSE)
-  consensus <- sapply(msa_out, .process_msa)
+
+  if(run_msa){
+    msa_out <- mapply(function(x,y)t_coffee(c(original_vec[x],y)),
+                      x = member_idx, y = vec[non_solo_id], SIMPLIFY = FALSE)
+    consensus <- sapply(msa_out, .process_msa)
+    names(msa_out) <- non_solo_id
+  }else{
+    consensus <- vec
+  }
   names(consensus) <- non_solo_id
-  names(msa_out) <- non_solo_id
   consensus <- unlist(consensus)
   if(add_id & length(consensus) > 0L){
     names(consensus) <- UUIDgenerate(n = length(consensus))
@@ -76,51 +103,6 @@ greedy_scs <- function(vec, n_reads = NULL, msa_result = FALSE, return_no_assemb
   }else{
     return(list(consensus, n_reads))
   }
-}
-
-# Finding the overlapping percentage and overlapping length between pair
-.overlap_reads <- function(vec, min_pid = 85, min_len = 10L)
-# This function calculates all the overlapping length between all the reads inside the vector.
-{
-  if(is.null(names(vec))){
-    names(vec) <- seq_along(vec)
-  }
-  if(length(vec)<=1){
-    # return empty data.table
-    return(data.table(
-      "seq1"=character(),
-      "seq2"=character(),
-      "seq1_left_clipped_len" = integer(),
-      "seq1_right_clipped_len" = integer(),
-      "seq2_left_clipped_len" = integer(),
-      "seq2_right_clipped_len" = integer(),
-      "seq1_len" = integer(),
-      "seq2_len" = integer(),
-      "seq1_ol_start" = integer(),
-      "seq1_ol_end" = integer(),
-      "seq2_ol_start" = integer(),
-      "seq2_ol_end" = integer(),
-      "pid" = numeric(),
-      "n_match" = integer(),
-      "aln_ol_len" = integer()
-      ))
-  }
-  n_seq <- length(vec)
-  idx <- names(vec)
-  info <- data.table(t(combn(idx, 2)))
-  colnames(info) <- c("seq1", "seq2")
-  info <- cbind(info, do.call(cbind, overlapper(vec[info[[1]]], vec[info[[2]]])))
-  # Filtering abberent alignment
-  info <- info[info$pid > min_pid & info$aln_ol_len > min_len & (info$seq1_ol_start == 0L | info$seq2_ol_start == 0L), ]
-  info <- info[order(info$aln_ol_len, decreasing = TRUE), ]
-
-  # Converting to 1-based index
-  info$seq1_ol_start <- info$seq1_ol_start + 1L
-  info$seq1_ol_end <- info$seq1_ol_end + 1L
-  info$seq2_ol_start <- info$seq2_ol_start + 1L
-  info$seq2_ol_end <- info$seq2_ol_end + 1L
-  info <- .reorder_seq_info(info)
-  info
 }
 
 # Function to determine a pair of sequences that which one is of the left hand side
