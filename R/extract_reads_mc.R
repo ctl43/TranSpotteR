628e3d34931b1b54e2bca5a97eb05e996e696060
ctl43 <ctlaw@hku.hk>
2021-01-12 13:19:41 +0800

    Edited filters for extarcting usable read pairs

--- R/extract_reads_mc.R
+++ R/extract_reads_mc.R
@@ -3,27 +3,38 @@
 #' @importFrom BiocParallel bplapply MulticoreParam
 #' @importFrom data.table rbindlist data.table
 
-extract_info_reads <- function(bam, readin = 2.5E6, tmp_dir, mc.cores = 12L, out_dir){
+extract_info_reads <- function(bam, readin = 2.5E6, tmp_dir, threads = 8L,
+                               out_dir, chromosome = c(1:22, "X", "Y", "KJ173426")){
   # bam <- "/home/ctlaw/synology/HKU_88/bwa_output/HKU_liver_43/HKU_liver_tumor_43/ERR093456.bam"
   # tmp_dir <- "/home/ctlaw/dicky/analysis/Enhancer_hijack/temp"
-  tag <- gsub(".bam", "", basename(bam))
-  txt <- file.path(tmp_dir, paste0(tag, ".txt"))
+  tag <- gsub(".bam$", "", basename(bam))
+
+  if (is.null(tmp_dir)) {
+    tempdir()
+    tmp_dir <- tempfile()
+    dir.create(tmp_dir)
+    on.exit(unlink(tmp_dir, recursive = TRUE))
+  }
+
+  sorted_sam <- file.path(tmp_dir, paste0(tag, ".sam"))
+
   print(paste(Sys.time(), "Sorting reads"))
-  cmd <- paste("(samtools sort -@", mc.cores,"-n -m 1500M -o /dev/stdout", bam, "|samtools view -h /dev/stdin", ")>", txt, collapse = " ")
+  cmd <- paste("(samtools sort -@", threads,"-n -m 1500M -o -", bam, "|samtools view -h -", ")>", sorted_sam, collapse = " ")
   system(cmd)
-  n_row <- system(paste0("wc -l ", txt), intern = TRUE)
+  # txt <- "/home/ctlaw/dicky/analysis/Enhancer_hijack/temp/ERR093456.sam"
+  n_row <- system(paste0("wc -l ", sorted_sam), intern = TRUE)
   n_row <- as.integer(gsub(" .*", "", n_row))
   n_skip <- seq(0, n_row, by = readin)
 
   print(paste(Sys.time(), "Importing reads and extracting informative reads"))
-  system.time(info <- bplapply(n_skip[1], function(x){
-    reads_txt <- read_sam(txt, start = x + 1L, nrow = 2.5e6, select = c(1:6, 10))
-    info <- get_info(reads_txt)
-  }, BPPARAM =  MulticoreParam(workers = 12L)))
+  info <- bplapply(n_skip, function(x){
+    reads_txt <- read_sam(sorted_sam, start = x + 1L, nrow = readin, select = c(1:6, 10))
+    info <- get_info(reads_txt, chromosome = chromosome)
+  }, BPPARAM =  MulticoreParam(workers = threads))
 
   # Extracting information from head and tail reads
   extra <- do.call(rbind, lapply(info, "[[", "head_tail_reads"))
-  extra_info <- .extract_informative_reads(extra)
+  extra_info <- .extract_informative_reads(extra, chromosome = chromosome)
 
   # Exporting discordant reads
   discordant <- rbindlist(lapply(info, "[[", "discordant"))
@@ -37,7 +48,7 @@ extract_info_reads <- function(bam, readin = 2.5E6, tmp_dir, mc.cores = 12L, out
   # rs_diff_q <- rname_strand$QNAME[rname_strand$n_rname != 1]#|rname_strand$n_strand != 1]
   # discordant[discordant$QNAME %in% c(rs_diff_q, pos_diff_q)]
   disc_out <- file.path(out_dir, paste0(tag, "_disc.txt"))
-  write.table(discordant, disc_out, quote=FALSE, col.names=TRUE, row.names=FALSE, sep="\t")
+  write.table(discordant, disc_out, quote = FALSE, col.names = TRUE, row.names = FALSE, sep = "\t")
 
   # Exporting reads mapped to multiple regions
   multi_mapped <- rbindlist(lapply(info, "[[", "multi_mapped"))
@@ -45,34 +56,13 @@ extract_info_reads <- function(bam, readin = 2.5E6, tmp_dir, mc.cores = 12L, out
   mm_out <- file.path(out_dir, paste0(tag, "_mm.txt"))
   write.table(multi_mapped, mm_out, quote = FALSE, col.names=TRUE, row.names=FALSE, sep="\t")
 
-  print(paste(Sys.time(), "Mapping multi mapped reads to line1 and polyA"))
-  # # Identifying reads mapped to line1
-  # non_supp_second <- multi_mapped[ !bitwAnd(multi_mapped$FLAG, 0x100) & !bitwAnd(multi_mapped$FLAG, 0x800),]
-  # multi_mapped_seq <- DNAStringSet(non_supp_second$SEQUENCE)
-  # is_first <- !bitwAnd(non_supp_second$FLAG, 0x40)
-  # names(multi_mapped_seq) <- paste0(non_supp_second$QNAME)
-  # mm_fa_out <- file.path(out_dir, paste0(tag, "_multi_mapped.fasta"))
-  # writeFasta(multi_mapped_seq, mm_fa_out)
-  # cmd <- paste("bwa mem -t 5", "/home/ctlaw/dicky/reference/fasta/line1_reference/L1complete.fa", mm_fa_out,
-  #        "|samtools view -F 4 -|cut -f 1,2,3,4,5,6,10")
-  # mm_aln <- read.delim(text=system(cmd, intern = TRUE),stringsAsFactors = FALSE, header=FALSE)
-  # line1_mm <- multi_mapped[multi_mapped$QNAME%in%mm_aln$V1,]
-  # line1_mm_out <- file.path(out_dir, paste0(tag, "_line1_mm.txt"))
-  # write.table(line1_mm, line1_mm_out, quote=FALSE, col.names=TRUE, row.names=FALSE, sep="\t")
-  #
-  # # Exporting line1 anchor (coverting it to bed format)
-  # line1_anchor <- line1_mm[!bitwAnd(line1_mm$FLAG, 0x100)&!bitwAnd(line1_mm$FLAG, 0x800)&line1_mm$MAPQ>=5,]
-  # line1_anchor_out <- file.path(out_dir, paste0(tag, "_line1_anchor.txt"))
-  # write.table(line1_anchor, line1_anchor_out, quote=FALSE, col.names=TRUE, row.names=FALSE, sep="\t")
-
-
   # Cleaning up
-  system(paste("rm",txt, collapse = " "))
+  system(paste("rm", sorted_sam, collapse = " "))
 }
 
 
 #' @export
-get_info <- function(input){
+get_info <- function(input, chromosome = NULL){
   readin <- N <- nrow(input)
   # Getting tail reads
   last_qname <- input[readin, "QNAME"]
@@ -89,29 +79,35 @@ get_info <- function(input){
   }
   head_reads <- input[1:Q,]
   input <- input[-c(1:Q, N:readin),]
-  informative_reads <- .extract_informative_reads(input)
+  system.time(informative_reads <- .extract_informative_reads(input, chromosome = chromosome))
   c(head_tail_reads=list(rbind(head_reads, tail_reads)), informative_reads)
 }
 
 #' @importFrom data.table rbindlist data.table
 #' @importFrom Rsamtools bamFlagAsBitMatrix
-.extract_informative_reads <- function(seq_info){
+.extract_informative_reads <- function(seq_info, chromosome = NULL){
   # Converting sam flag to readable matrix
   mat <- bamFlagAsBitMatrix(seq_info[["FLAG"]])
 
   # Gathering information for statisitc analysis
+  if(!is.null(chromosome)){
+    non_target_RNAME <- !seq_info[["RNAME"]] %in% chromosome
+  }
+
   mat <- data.table(QNAME = seq_info[["QNAME"]], count = 1,
                     highMAPQ = seq_info[["MAPQ"]] > 5,
                     lowMAPQ = seq_info[["MAPQ"]] <= 5,
-                    # non_target_RNAME =! seq_info[["RNAME"]] %in% c(1:22, "X", "Y", "KJ173426"),
+                    non_target_RNAME = non_target_RNAME,
                     mat)
+  mat$targetIsHigh <- (mat$highMAPQ & !mat$non_target_RNAME)
 
   # Gathering flag statistics in a paired reads way
   paired_flag <- mat[, list(count = sum(count), highMAPQ = sum(highMAPQ),
                             lowMAPQ = sum(lowMAPQ), isProperPair = sum(isProperPair),
                             isSupplementaryAlignment = sum(isSupplementaryAlignment),
-                            # haveNonTargetRname = sum(non_target_RNAME),
-                            isUnmappedQuery = sum(isUnmappedQuery)), by = "QNAME"]
+                            haveNonTargetRname = sum(non_target_RNAME),
+                            isUnmappedQuery = sum(isUnmappedQuery),
+                            targetIsHigh = sum(targetIsHigh)), by = "QNAME"]
 
   # Getting not interested reads
   all_low <- paired_flag$"count" == paired_flag$"lowMAPQ" # reads with a pair of low mapq
@@ -119,19 +115,29 @@ get_info <- function(input){
     paired_flag$"isSupplementaryAlignment" == 0 &
     paired_flag$"isProperPair" == paired_flag$count &
     paired_flag$"isUnmappedQuery" == 0
-  # have_non_target <- paired_flag[["haveNonTargetRname"]] > 0
-  unwanted <- all_proper | all_low | have_non_target
+  both_non_target <- paired_flag[["haveNonTargetRname"]] == paired_flag$count # both reads are on the non target chromosom
+  unwanted <- all_proper | all_low | both_non_target
 
   # Getting reads with at least one with high mapq and one with low mapq
   is_high_low_mapq <- paired_flag[["highMAPQ"]] != 0 &
     (paired_flag[["lowMAPQ"]] != 0 | paired_flag[["isUnmappedQuery"]] != 0) &
-    !unwanted
+    paired_flag[["targetIsHigh"]] > 0 & (!unwanted)
   high_low_mapq <- paired_flag[["QNAME"]][is_high_low_mapq]
-  high_low_mapq <- seq_info[seq_info[["QNAME"]] %in% high_low_mapq,]
+
+  # Getting reads with long Clipped sequence
+  sh_loc <- grep("S|H",seq_info$CIGAR, perl = TRUE)
+  sh_data <- seq_info[sh_loc,]
+  is_left_long_clip <- .get_clip_length(sh_data$CIGAR) > 10
+  is_right_long_clip <- .get_clip_length(sh_data$CIGAR, start = FALSE) > 10
+  sh_q <- sh_data$QNAME[is_left_long_clip|is_right_long_clip]
+  interested <- c(high_low_mapq, sh_q)
+  interested <- interested[!interested %in% paired_flag[["QNAME"]][unwanted]]
+  high_low_mapq <- seq_info[seq_info[["QNAME"]] %in% interested,]
 
   # Getting discor reads
   is_high_high_discor <- paired_flag[["count"]] == paired_flag[["highMAPQ"]] &
-    (paired_flag[["isProperPair"]] != paired_flag[["count"]] | paired_flag[["isSupplementaryAlignment"]] > 0) &
+    (paired_flag[["isProperPair"]] != paired_flag[["count"]] |
+       paired_flag[["isSupplementaryAlignment"]] > 0) &
     !unwanted
   high_high_discor <- paired_flag[["QNAME"]][is_high_high_discor]
   high_high_discor <- seq_info[seq_info[["QNAME"]] %in% high_high_discor,]
