f3280fdc2506d43a82f3c4af1ec93e3c35696286
ctl43 <ctlaw@hku.hk>
2021-03-06 19:43:59 +0800

    Included filter_read_by_coverage.R

--- R/clustering_reads.R
+++ R/clustering_reads.R
@@ -5,11 +5,11 @@
 
 clustering_reads <- function(total, min_peak = 5, size_tol = 0, max_reads = 100){
   gr <- total[total$is_anchor]
-  p <- gr[strand(gr)=="+"]
-  m <- gr[strand(gr)=="-"]
-  p_cluster <- filter_read_by_coverage(p, min_peak = min_peak)
+  p <- gr[strand(gr) == "+"]
+  m <- gr[strand(gr) == "-"]
+  p_cluster <- .filter_read_by_coverage(p, min_peak = min_peak)
   names(p_cluster) <- seq_along(p_cluster)
-  m_cluster <- filter_read_by_coverage(m, min_peak = min_peak)
+  m_cluster <- .filter_read_by_coverage(m, min_peak = min_peak)
   names(m_cluster) <- seq_along(m_cluster)
   strand(p_cluster) <- "+"
   strand(m_cluster) <- "-"
@@ -38,11 +38,11 @@ clustering_reads <- function(total, min_peak = 5, size_tol = 0, max_reads = 100)
   m <- m[m_ol@to]
   m_is_large <- table(m_group) > max_reads
   m_grp <- data.table(which = seq_along(m), grp = m_group)
-  m_large_which <- m_grp[m_grp$grp%in%which(m_is_large)]
+  m_large_which <- m_grp[m_grp$grp %in% which(m_is_large)]
   set.seed(20190721)
   m_selected_large <- m_large_which[, list(which = sample(which, max_reads)), by = grp]
   m_ok_group <- which(!m_is_large)
-  m_selected <- sort(c(m_selected_large$which, m_grp$which[m_grp$grp%in%m_ok_group]))
+  m_selected <- sort(c(m_selected_large$which, m_grp$which[m_grp$grp %in% m_ok_group]))
   m_selected <- m_grp[m_selected]
   m <- m[m_selected$which]
   m_partner <- total[.get_partner_read_id(m$QNAME_id)]
@@ -59,4 +59,36 @@ clustering_reads <- function(total, min_peak = 5, size_tol = 0, max_reads = 100)
   out[is_first] <- sub("_1", "_2", x[is_first])
   out[!is_first] <- sub("_2", "_1", x[!is_first])
   out
+}
+
+#' @export
+#' @importFrom GenomicRanges GRanges
+#' @importFrom S4Vectors 'elementMetadata<-' Rle
+#' @importFrom IRanges coverage slice subsetByOverlaps IntegerList
+#' @importFrom csaw mergeWindows
+#' @importFrom GenomeInfoDb seqinfo
+
+.filter_read_by_coverage <- function(x, min_read = 2,
+                                    min_peak = 5,
+                                    window_tol = 500,
+                                    rescue_tol = 500,
+                                    rescue = FALSE){
+  cvg <- coverage(x)
+  sliced <- slice(cvg, min_read)
+  max_count <- IntegerList(lapply(sliced, max))
+  chr <- Rle(names(sliced), lengths(sliced))
+  collapsed <- GRanges(seqnames = chr,
+                       IRanges(unlist(start(sliced)), unlist(end(sliced))),
+                       seqinfo = seqinfo(x))
+  collapsed$max_count <- unlist(max_count)
+  peaks <- collapsed[collapsed$max_count >= min_peak]
+  if(rescue){
+    temp <- collapsed[!collapsed$max_count >= min_peak] # Recusing reads that are around the main clusters
+    rescued <- subsetByOverlaps(temp, peaks + rescue_tol)
+    collapsed <- c(peaks, rescued)
+  }else{
+    collapsed <- peaks
+  }
+  collapsed <- mergeWindows(collapsed, tol = window_tol)$regions # Merge cluster within the tolerated range
+  collapsed
 }
