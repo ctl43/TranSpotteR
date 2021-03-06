7484f318d2a29595ae784807726786dc3e52f4c0
ctl43 <ctlaw@hku.hk>
2021-03-06 19:27:46 +0800

    Down sampling the number of reads in cluster to avoid excessive computational power in assembling reads. (Deafult: 100x)

--- R/clustering_reads.R
+++ R/clustering_reads.R
@@ -3,8 +3,8 @@
 #' @importFrom S4Vectors 'elementMetadata<-'
 #' @importFrom BiocGenerics strand start end 'strand<-'
 
-clustering_reads <- function(total, min_peak = 5, size_tol = 0){
-  gr <- total[total$is_anchor | total$is_disc]
+clustering_reads <- function(total, min_peak = 5, size_tol = 0, max_reads = 100){
+  gr <- total[total$is_anchor]
   p <- gr[strand(gr)=="+"]
   m <- gr[strand(gr)=="-"]
   p_cluster <- filter_read_by_coverage(p, min_peak = min_peak)
@@ -18,19 +18,37 @@ clustering_reads <- function(total, min_peak = 5, size_tol = 0){
   ## For positive clusters
   p_ol <- findOverlaps(p_cluster, p)
   p_group <- p_ol@from
-  names(p_group) <- p[p_ol@to]$QNAME
-  p_cluster$members <- split(p[p_ol@to], p_group)
+  p <- p[p_ol@to]
+  p_is_large <- table(p_group) > max_reads
+  p_grp <- data.table(which = seq_along(p), grp = p_group)
+  p_large_which <- p_grp[p_grp$grp %in% which(p_is_large)]
+  set.seed(20190721)
+  p_selected_large <- p_large_which[, list(which = sample(which, max_reads)), by = grp]
+  p_ok_group <- which(!p_is_large)
+  p_selected <- sort(c(p_selected_large$which, p_grp$which[p_grp$grp %in% p_ok_group]))
+  p_selected <- p_grp[p_selected]
+  p <- p[p_selected$which]
+  p_partner <- total[.get_partner_read_id(p$QNAME_id)]
+  p_cluster$members <- split(p, p_selected$grp)
+  p_cluster$partners <- split(p_partner, p_selected$grp)
 
   ## For negative clusters
   m_ol <- findOverlaps(m_cluster, m)
   m_group <- m_ol@from
-  names(m_group) <- m[m_ol@to]$QNAME
-  m_cluster$members <- split(m[m_ol@to], m_group)
+  m <- m[m_ol@to]
+  m_is_large <- table(m_group) > max_reads
+  m_grp <- data.table(which = seq_along(m), grp = m_group)
+  m_large_which <- m_grp[m_grp$grp%in%which(m_is_large)]
+  set.seed(20190721)
+  m_selected_large <- m_large_which[, list(which = sample(which, max_reads)), by = grp]
+  m_ok_group <- which(!m_is_large)
+  m_selected <- sort(c(m_selected_large$which, m_grp$which[m_grp$grp%in%m_ok_group]))
+  m_selected <- m_grp[m_selected]
+  m <- m[m_selected$which]
+  m_partner <- total[.get_partner_read_id(m$QNAME_id)]
+  m_cluster$members <- split(m, m_selected$grp)
+  m_cluster$partners <- split(m_partner, m_selected$grp)
   combined <- c(p_cluster, m_cluster)
-
-  # Getting partner reads
-  partner_ids <- .get_partner_read_id(unlist(combined$members, use.names = FALSE)$QNAME_id)
-  combined$partners <- split(total[partner_ids], rep(seq_along(combined), lengths(combined$members)))
   names(combined) <- seq_along(combined)
   combined
 }
