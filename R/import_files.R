f1ab41186c426bcd3ac722c268936b7a12414ed4
ctl43 <ctlaw@hku.hk>
2021-03-25 19:27:55 +0800

    Simplfied the script

--- R/import_files.R
+++ R/import_files.R
@@ -3,21 +3,23 @@
 #' @importFrom data.table fread
 #' @importFrom S4Vectors 'elementMetadata<-'
 #' @importFrom IRanges start end CharacterList IntegerList
-#' @importFrom  GenomicAlignments cigarWidthAlongReferenceSpace
+#' @importFrom GenomicAlignments cigarWidthAlongReferenceSpace
+#' @importFrom data.table rbindlist fread
 
-import_files <- function(extracted, include_unmapped = TRUE,
-                         anchor_min_mapq = 10, seq_info){
+import_files <- function(extracted, seq_info){
+  what <- c("character","integer","character", "integer", "integer",
+            "character", "character", "character")
+  x <- rbindlist(lapply(x, fread, colClasses = what))
   dt <- .internal_import(extracted)
   # Determine split reads (multi and unique mapped reads are on the same reads)
-  dt$is_anchor <- dt$MAPQ >= anchor_min_mapq & !dt$is_supp
   dt <- convertingHtoS(dt) # Converting all hard clipped to soft clipped
   dt[dt$CIGAR=="*", ]$RNAME <- "UNMAPPED"
   dt[dt$CIGAR=="*", ]$POS <- 1
-  dt[dt$CIGAR=="*", ]$CIGAR <- paste0(nchar(dt$SEQUENCE[dt$CIGAR=="*"]),"M")
+  dt[dt$CIGAR=="*", ]$CIGAR <- paste0(nchar(dt$SEQUENCE[dt$CIGAR=="*"]), "M")
   seq_info@seqnames <- c(seq_info@seqnames, "UNMAPPED")
   seq_info@seqlengths <- c(seq_info@seqlengths, 99999L)
   seq_info@is_circular <- c(seq_info@is_circular, FALSE)
-  seq_info@genome <- c(seq_info@genome, "hs37d5_KJ173426")
+  seq_info@genome <- c(seq_info@genome, seq_info@genome[1])
   gr <- GRanges(seqnames = dt$RNAME,
                 IRanges(dt$POS, width = cigarWidthAlongReferenceSpace(dt$CIGAR)),
                 strand = ifelse(bitwAnd(dt$FLAG, 16), "-", "+"),
@@ -25,15 +27,4 @@ import_files <- function(extracted, include_unmapped = TRUE,
   names(gr) <- dt$QNAME_id
   elementMetadata(gr) <- dt
   gr
-}
-
-#' @export
-#' @importFrom data.table rbindlist fread
-.internal_import <- function(x){
-  what <- c("character","integer","character", "integer", "integer", "character", "character", "character")
-  x <- rbindlist(lapply(x, fread, colClasses = what))
-  x$is_first <- !!bitwAnd(x$FLAG, 0x40)
-  x$is_rc <- !!bitwAnd(x$FLAG, 0x10)
-  x$is_supp <- !!bitwAnd(x$FLAG, 0x800)
-  x
 }
