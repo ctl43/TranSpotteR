55a006f96addc183d1c71dcb44e605048a08341c
ctl43 <ctlaw@hku.hk>
2021-03-29 15:56:46 +0800

    Fixed bugs caused by empty entry

--- R/infer_simple_insertion.R
+++ R/infer_simple_insertion.R
@@ -1,6 +1,11 @@
 #' @export
-infer_simple_insertion <- function(a, chromosome, tol = 10000){
-  grl <- .preprocess_info(a, chromosome = chromosome)
+infer_simple_insertion <- function(a, chromosome, target, tol = 10000){
+  grl <- .preprocess_info(a, chromosome = c(chromosome, target), tol = tol)
+  grl <- grl[any(S4Vectors::match(seqnames(grl), target, nomatch = 0) > 0)]
+  cluster_regions <- convert_character2gr(elementMetadata(grl)$cluster_region)
+  grl <- grl[!S4Vectors::match(seqnames(cluster_regions), target, nomatch = 0) > 0]
+  grl <- .get_the_best(grl, elementMetadata(grl)$cluster_region)
+  # names(grl) <- elementMetadata(grl)$cluster_region
 
   # Finding annotation that fully cover the insertion and they will be handled separately
   first_gr <- unlist(first_element(grl))
@@ -9,49 +14,61 @@ infer_simple_insertion <- function(a, chromosome, tol = 10000){
   first_last_gr <- split(first_last_gr, as.integer(first_last_gr$origin))
   head_tail_ol <- how_many_regions_in_range(first_last_gr, tol = tol) == 1
   fully_covered <- grl[head_tail_ol]
-  fully_covered_ranges <- split(fully_covered_ranges, strand(fully_covered_ranges))
-  fully_covered_ranges$`-` <- subsetByOverlaps(fully_covered_ranges$`-`, fully_covered_ranges$`+` + tol, ignore.strand = TRUE, invert = TRUE)
-  fully_covered_ranges <- unlist(fully_covered_ranges)
-  fully_covered <- fully_covered[fully_covered_ranges$origin]
   storage_1 <- .simple_initialize_storage(length(fully_covered))
-  storage_1 <- simple_data_input(storage_1, fully_covered, direction = "left")
-  storage_1 <- simple_data_input(storage_1, fully_covered, direction = "right")
+  if(length(fully_covered) != 0){
+    storage_1 <- simple_data_input(storage_1, fully_covered, direction = "left", targets = target)
+    storage_1 <- simple_data_input(storage_1, fully_covered, direction = "right", targets = target)
+  }
 
   # paired insertion
   p_clusters <- first_gr[!head_tail_ol & first_gr$origin_direction == "+"]
   m_clusters <- last_gr[!head_tail_ol & last_gr$origin_direction == "-"]
-  paired <- pairing_annotation(p_clusters, m_clusters)
-  storage_2 <- .initialize_storage(length(paired))
-  storage_2 <- data_input(storage_2, paired@first, direction = "left")
-  storage_2 <- data_input(storage_2, paired@second, direction = "right")
-  storage_2$n_reads_5p <- elementMetadata(paired@first)$nreads
-  storage_2$n_reads_3p <- elementMetadata(paired@second)$nreads
+  if(length(p_clusters) > 0 & length(m_clusters) > 0){
+    paired <- pairing_annotation(p_clusters, m_clusters)
+  }else{
+    paired <- Pairs(first = NULL, second = NULL)
+  }
+
+  storage_2 <- .simple_initialize_storage(length(paired))
+  grl_first <- grl[as.character(paired@first$origin)]
+  grl_second <- grl[as.character(paired@second$origin)]
+
+  if(length(paired) != 0){
+    storage_2 <- simple_data_input(storage_2, grl_first, direction = "left", targets = target)
+    storage_2 <- simple_data_input(storage_2, grl_second, direction = "right", targets = target)
+    storage_2$n_reads_5p <- elementMetadata(grl_first)$nreads
+    storage_2$n_reads_3p <- elementMetadata(grl_second)$nreads
+  }
 
   # unpaired positive cluster
-  used_origins <- c(used_origins, as.character(c(fully_covered$origin, paired@first$origin, paired@second$origin)))
+  used_origins <- as.character(c(unlist(fully_covered)$origin, paired@first$origin, paired@second$origin))
   unpaired <- grl[!(names(grl) %in% used_origins)]
   unpaired_is_p <- unlist(first_element(unpaired))$origin_direction == "+"
   unpaired_p <- unpaired[unpaired_is_p]
   storage_3 <- .simple_initialize_storage(sum(unpaired_is_p))
-  storage_3 <- simple_data_input(storage_3, unpaired_p, direction = "left")
-
+  if(length(unpaired_p) != 0){
+    storage_3 <- simple_data_input(storage_3, unpaired_p, direction = "left", targets = target)
+  }
   # unpaired negative cluster
   unpaired_m <- unpaired[!unpaired_is_p]
-  storage_4 <- .initialize_storage(sum(!unpaired_is_p))
-  storage_4 <- data_input(storage_4, unpaired[!unpaired_is_p], direction = "right")
+  storage_4 <- .simple_initialize_storage(sum(!unpaired_is_p))
+  if(length(unpaired_m) != 0){
+    storage_4 <- simple_data_input(storage_4, unpaired_m, direction = "right", targets = target)
+  }
 
   collected_origins <- split(unlist(first_element(fully_covered))$origin, seq_along(fully_covered))
-  collected_origins <- c(collected_origins, split(c(paired@first$origin, paired@second$origin), rep(seq_along(paired), 2)))
+  if(length(paired) > 0){
+    collected_origins <- c(collected_origins, split(c(paired@first$origin, paired@second$origin), rep(seq_along(paired), 2)))
+  }
   collected_origins <- c(collected_origins, split(unlist(first_element(unpaired_p))$origin, seq_along(unpaired_p)))
   collected_origins <- c(collected_origins, split(unlist(first_element(unpaired_m))$origin, seq_along(unpaired_m)))
   collected_origins <- IntegerList(collected_origins)
-
   storage <- rbind(storage_1, storage_2, storage_3, storage_4)
   return(list(collected_origins, storage))
 }
 
 
-simple_data_input <- function(storage, data, direction){
+simple_data_input <- function(storage, data, direction, targets){
   if(nrow(storage) != length(data)){
     stop()
   }
@@ -119,6 +136,9 @@ simple_data_input <- function(storage, data, direction){
                   "is_exact_3p" = NA,
                   "n_reads_3p" = NA)
   setDT(storage)
+  if(n == 0){
+    return(storage[-1,])
+  }
   storage <- rep(list(storage), n)
   rbindlist(storage)
 }
